fe79560129697f1d2927bc6894ac60a014c4dae0
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-03-29 23:33:18 -0300

    Add API jsonplaceholder for show monsters name

--- src/App.js
+++ src/App.js
@@ -6,41 +6,26 @@ class App extends Component {
    constructor() {
       super();
       this.state = {
-         monsters: [
-            {
-               id: 1,
-               name: 'Linda'
-            },
-            {
-               id: 2,
-               name: 'Frankl'
-            },
-            {
-               id: 3,
-               name: 'Jacky'
-            },
-            {
-               id: 4,
-               name: 'Andrei'
-            }
-         ]
+         monsters: []
       };
    }
 
+   componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+         .then((response) => response.json())
+         .then((users) => this.setState(() => {
+            return {monsters: users}
+         }, () => console.log(this.state)))
+   }
+
    render() {
-      return (
-         <div className="App">
-            {
-               this.state.monsters.map((monster) => {
-                  return (
-                     <dov key={monster.id}>
-                        <h1>{monster.name}</h1>
-                     </dov>
-                  )
-               })
-            }
-         </div>
-      );
+      return (<div className="App">
+            {this.state.monsters.map((monster) => {
+               return (<div key={monster.id}>
+                     <h1>{monster.name}</h1>
+                  </div>)
+            })}
+         </div>);
    }
 }
 
