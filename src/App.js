54bd4a0eeea8cd4a7e85c9104e4244632e622503
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-04-27 02:55:55 -0300

    :construction: Implement filter monster for search v1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import {Component} from "react";
-import "./App.css";
+import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
 
-   constructor() {
-      super();
-      this.state = {
-         monsters: []
-      };
-   }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
+  }
 
-   componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/users')
-         .then((response) => response.json())
-         .then((users) => this.setState(() => {
-            return {monsters: users}
-         }, () => console.log(this.state)))
-   }
+  render() {
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Procurar um monstro..."
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
 
-   render() {
-      return (<div className="App">
-         <input className='search-box' type='search' placeholder="Procurar um monstro..." onChange={(event) => {
-            console.log(event.target.value)
-         }}/>
-         {this.state.monsters.map((monster) => {
-            return (<div key={monster.id}>
-               <h1>{monster.name}</h1>
-            </div>)
-         })}
-      </div>);
-   }
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
